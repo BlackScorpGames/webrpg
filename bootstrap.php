bcce28059e1fc5ff050c638166808c4d20326eec
BlackScorp <witalimik@web.de>
2017-02-28 11:14:28 +0100

    enabled modules and added is logged in checks

--- bootstrap.php
+++ bootstrap.php
@@ -6,12 +6,23 @@
 require_once __DIR__ . '/source/event.php';
 require_once __DIR__ . '/source/router.php';
 
+require_once __DIR__ . '/source/account/index.php'; //include account module
+require_once __DIR__ . '/source/map/index.php'; //include map module
 
 router('/', function () {
     echo "Hello World";
 });
 
-event('http.404', null, function ($path) {
+/**
+ * Setup basic events
+ */
+event('http.403', [], function () {
+    header('Content-Type:text/html;charset=utf-8');
+    header('HTTP/1.0 403 Forbidden');
+    echo 'You are not logged in, please <a href="login">login</a> first';
+});
+
+event('http.404', [], function ($path) {
     header('Content-Type:text/html;charset=utf-8');
     header('HTTP/1.0 404 Not Found');
     echo sprintf("Path '%s' not found", $path);
--- source/map/routes.php
+++ source/map/routes.php
@@ -1,1 +1,9 @@
 <?php
+
+router('/map', function () {
+    if (!isLoggedIn()) {
+        return event('http.403');
+    }
+
+    return 'This is a map';
+});
