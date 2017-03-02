654b03c7d902b2a175940edf6bb82c2420483518
BlackScorp <witalimik@web.de>
2017-03-02 11:15:30 +0100

    playing around with styles

--- templates/default/index.php
+++ templates/default/index.php
@@ -8,23 +8,26 @@
 
 
 <?php section('content') ?>
-
+    <nav class="form-group">
+        <a class="button">News</a>
+        <a class="button">About</a>
+    </nav>
     <div class="grid-40">
         <?php if (count($errors) > 0): ?>
             <div class="error">
                 <?php foreach ($errors as $message): ?>
-                    <p><?= $message?></p>
+                    <p><?= $message ?></p>
                 <?php endforeach; ?>
             </div>
         <?php endif; ?>
         <form method="POST" action="/login">
-            <div class="grid-100 grid-parent">
+            <div class="grid-100 grid-parent form-group">
                 <label class="grid-30 grid-parent" for="username"><?= _('Username') ?></label>
                 <input class="grid-70 grid-parent" id="username" type="text" name="username"
                        placeholder="<?= _('Username') ?>"
                        value="<?= $username ?>">
             </div>
-            <div class="grid-100 grid-parent">
+            <div class="grid-100 grid-parent form-group">
                 <label class="grid-30 grid-parent" for="password"><?= _('Password') ?></label>
                 <input class="grid-70 grid-parent" id="password" type="password" name="password"
                        placeholder="<?= _('Password') ?>"
