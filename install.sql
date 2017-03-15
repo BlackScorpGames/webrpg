93955554737cf694279455eda7fe946c06134d9d
BlackScorp <witalimik@web.de>
2017-03-15 13:29:20 +0100

    fixed #2

--- install.sql
+++ install.sql
@@ -1,42 +1,21 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server Version:               10.1.20-MariaDB - mariadb.org binary distribution
--- Server Betriebssystem:        Win64
--- HeidiSQL Version:             9.4.0.5125
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `users` (
+  `userId` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `registrationDate` datetime DEFAULT NULL,
+  `lastAction` datetime DEFAULT NULL,
+  PRIMARY KEY (`userId`),
+  UNIQUE KEY `unique` (`email`,`username`)
+) ENGINE=InnoDB COLLATE=utf8_unicode_ci;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- Exportiere Struktur von Tabelle webrpg.characters
 CREATE TABLE IF NOT EXISTS `characters` (
   `characterId` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
+  `name` varchar(32) NOT NULL,
   `userId` int(11) unsigned NOT NULL,
   `lastAction` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`characterId`),
   UNIQUE KEY `name` (`name`),
   KEY `FK_USER` (`userId`),
   CONSTRAINT `FK_USER` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
--- Daten Export vom Benutzer nicht ausgewählt
--- Exportiere Struktur von Tabelle webrpg.users
-CREATE TABLE IF NOT EXISTS `users` (
-  `userId` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(254) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(254) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `registrationDate` datetime DEFAULT NULL,
-  `lastAction` datetime DEFAULT NULL,
-  PRIMARY KEY (`userId`),
-  UNIQUE KEY `unique` (`email`,`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Daten Export vom Benutzer nicht ausgewählt
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+) ENGINE=InnoDB COLLATE=utf8_unicode_ci;
