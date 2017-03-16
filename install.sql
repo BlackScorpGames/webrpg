da41df30a510f382195a336cddb2fe6e72cf2e01
BlackScorp <witalimik@web.de>
2017-03-16 20:56:12 +0100

    disabled unique key for email because of varchar limit and UTF-8
    seee here http://stackoverflow.com/questions/15157227/mysql-varchar-index-length#answer-16474039

--- install.sql
+++ install.sql
@@ -1,14 +1,16 @@
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `userId` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `email`  varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `registrationDate` datetime DEFAULT NULL,
   `lastAction` datetime DEFAULT NULL,
   PRIMARY KEY (`userId`),
-  UNIQUE KEY `unique` (`email`,`username`)
-) ENGINE=InnoDB COLLATE=utf8_unicode_ci;
+  UNIQUE KEY `unique_username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `characters`;
 CREATE TABLE IF NOT EXISTS `characters` (
   `characterId` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
@@ -17,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `characters` (
   `gender` tinyint(1) unsigned NOT NULL,
   `lastAction` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`characterId`),
-  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `unique_name` (`name`),
   KEY `FK_USER` (`userId`),
   CONSTRAINT `FK_USER` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
