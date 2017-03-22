46dd78a8b0d7bdae3aa9f5142ab0d16b0d583602
Vitalij Mik <witalimik@web.de>
2017-03-22 21:52:54 +0100

    remove unused test route

--- source/inventory/events.php
+++ source/inventory/events.php
@@ -1,15 +1,4 @@
 <?php
-
-router('/testNewChar', function () {
-    $newCharacter = [
-        'id' => 1,
-        'name' => 'foo',
-        'class' => 'archer',
-        'gender' => 'male'
-    ];
-    event('game.newCharacter', $newCharacter);
-});
-
 event('game.newCharacter', [], function ($characterId, $characterName, $class, $gender) {
     $key = sprintf('%s.%s', $gender, $class);
     $initialEquipmentConfig = config('initialEquipment');
@@ -23,5 +12,5 @@ event('game.newCharacter', [], function ($characterId, $characterName, $class, $
         $sql = "INSERT INTO inventory(characterId,slot,itemName,amount) VALUES(".$characterId.",".$slot.",'".$itemName."',1)";
         mysqli_query($db,$sql);
     }
-    var_dump($initialEquipment);
+
 });
