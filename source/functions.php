09d7f86c8926f05c971607c80899ad7cdcf6b7ad
Magnus Reiß <magnus.reiss@doccheck.com>
2017-03-27 11:28:34 +0200

    add instanceof in getDb() fix redirect(), after redirect, the current runnig php process can be killed add php docs to functions add some bad words

--- source/functions.php
+++ source/functions.php
@@ -1,18 +1,27 @@
 <?php
+/**
+ * @return bool is post request
+ */
 function isPost()
 {
     return $_SERVER['REQUEST_METHOD'] === 'POST';
 }
 
+/**
+ * @return bool is get request
+ */
 function isGet()
 {
     return $_SERVER['REQUEST_METHOD'] === 'GET';
 }
 
+/**
+ * @param string $path
+ */
 function redirect($path)
 {
-    header('Location:' . $path);
-    return;
+    header('Location:'.$path);
+    exit(0);
 }
 
 /**
@@ -24,7 +33,8 @@ function getDb()
      * @var mysqli
      */
     static $mysqli = null;
-    if ($mysqli) {
+
+    if ($mysqli instanceof mysqli) {
         return $mysqli;
     }
 
@@ -35,9 +45,13 @@ function getDb()
         trigger_error(mysqli_connect_error());
     }
     mysqli_set_charset($mysqli, $charset);
+
     return $mysqli;
 }
 
+/**
+ * @param string $url
+ */
 function activateNavigation($url)
 {
     $navigationItems = sharedVariable('navigation');
@@ -53,13 +67,19 @@ function activateNavigation($url)
     sharedVariable('navigation', $navigationItems);
 }
 
+/**
+ * @param string|null $title
+ * @param string|null $url
+ *
+ * @return mixed
+ */
 function navigation($title = null, $url = null)
 {
     if ($title) {
         $item = [
             'title' => $title,
             'url' => $url,
-            'isActive' => false
+            'isActive' => false,
         ];
         $items = sharedVariable('navigation');
         if (!$items) {
@@ -73,36 +93,58 @@ function navigation($title = null, $url = null)
     if (!$navigation) {
         return [];
     }
+
     return $navigation;
 }
 
+/**
+ * @param string      $name
+ * @param string|null $value
+ *
+ * @return mixed
+ */
 function session($name, $value = null)
 {
     if (!$value && func_num_args() === 2) {
         unset($_SESSION[$name]);
+
         return;
     }
     if ($value) {
         return $_SESSION[$name] = $value;
     }
+
     return isset($_SESSION[$name]) ? $_SESSION[$name] : null;
 }
 
+/**
+ * @param string      $name
+ * @param string|null $value
+ *
+ * @return mixed
+ */
 function sharedVariable($name, $value = null)
 {
     static $variables = [];
     if ($value) {
         return $variables[$name] = $value;
     }
+
     return isset($variables[$name]) ? $variables[$name] : null;
 }
 
+/**
+ * @param string     $name
+ * @param array      $data
+ * @param mixed|null $action
+ *
+ * @return mixed
+ */
 function event($name, array $data = [], $action = null)
 {
     static $events = [];
 
     if ($action) {
-
         return $events[$name][] = $action;
     }
 
@@ -111,34 +153,48 @@ function event($name, array $data = [], $action = null)
             call_user_func_array($event, $data);
         }
     }
+
     return null;
 }
 
+/**
+ * @param string      $key
+ * @param string|null $value
+ *
+ * @return mixed
+ */
 function config($key, $value = null)
 {
     static $config = [];
     if ($value) {
         return $config[$key] = $value;
     }
+
     return isset($config[$key]) ? $config[$key] : null;
 }
 
+/**
+ * @return array
+ */
 function getBadWords()
 {
-    return require_once __DIR__ . '/../config/badwords.php';
+    return require_once __DIR__.'/../config/badwords.php';
 }
 
 /**
- * @param $path
+ * @param string               $path
  * @param closure|string|array $action
- * @return null
+ *
+ * @return mixed
  */
 function router($path, $action = null)
 {
     static $routes = [];
+
     if (!$path) {
         $path = '/';
     }
+
     if ($action) {
         return $routes[$path] = $action;
     }
@@ -152,13 +208,15 @@ function router($path, $action = null)
             }
 
             array_shift($match);
-            event('middleware.before',[$match]);
-            event('middleware.before.'.$route,[$match]);
+            event('middleware.before', [$match]);
+            event('middleware.before.'.$route, [$match]);
             call_user_func_array($action, $match);
-            event('middleware.after.'.$route,[$match]);
-            event('middleware.after',[$match]);
+            event('middleware.after.'.$route, [$match]);
+            event('middleware.after', [$match]);
+
             return null;
         }
     }
+
     return event('http.404', [$path, 'Route not found']);
 }
--- source/template.php
+++ source/template.php
@@ -1,8 +1,13 @@
 <?php
 
+/**
+ * @param string  $path
+ * @param mixed[] $data
+ *
+ * @return string
+ */
 function render($path, array $data = [])
 {
-
     $fileName = _getTemplateFile($path);
 
     _templateData('data', $data);
@@ -10,12 +15,18 @@ function render($path, array $data = [])
         extract($data, EXTR_SKIP);
     }
 
-
     ob_start();
     require_once $fileName;
+
     return ob_get_clean();
 }
 
+/**
+ * @param string $key
+ * @param mixed  $value
+ *
+ * @return mixed
+ */
 function _templateData($key, $value = null)
 {
     static $data = [];
@@ -27,25 +38,40 @@ function _templateData($key, $value = null)
     return isset($data[$key]) ? $data[$key] : [];
 }
 
+/**
+ * @param string $path
+ *
+ * @return string
+ */
 function _getTemplateFile($path)
 {
     $templateDirectories = config('templateDirectories');
 
     $fileName = '';
     foreach ($templateDirectories as $templateDirectory) {
-        $fileName = $templateDirectory . $path . '.php';
+        $fileName = $templateDirectory.$path.'.php';
         if (is_file($fileName)) {
             return realpath($fileName);
         }
     }
     if (!(bool)$fileName) {
-        trigger_error(sprintf('Template file "%s" not found in directories "%s"', $path . '.php', implode('","', $templateDirectories)));
+        trigger_error(
+            sprintf(
+                'Template file "%s" not found in directories "%s"',
+                $path.'.php',
+                implode('","', $templateDirectories)
+            )
+        );
     }
 
     return $fileName;
-
 }
 
+/**
+ * @param string $path
+ *
+ * @return mixed
+ */
 function layout($path)
 {
     static $layoutData = '';
@@ -56,14 +82,20 @@ function layout($path)
     $data = _templateData('data');
 
     echo render($path, $data);
+
+    return null;
 }
 
+/**
+ * @param string $name
+ *
+ * @return bool
+ */
 function section($name)
 {
     static $sections = [];
 
     if (!isset($sections[$name])) {
-
         return $sections[$name] = ob_start();
     }
     $content = ob_get_clean();
@@ -71,25 +103,38 @@ function section($name)
     $data[$name] = $content;
     _templateData('data', $data);
     unset($sections[$name]);
+
     return true;
 }
 
+/**
+ * @param string $name
+ *
+ * @return bool
+ */
 function sectionAppend($name)
 {
     static $sections = [];
 
     if (!isset($sections[$name])) {
         ob_clean();
+
         return $sections[$name] = ob_start();
     }
     $content = ob_get_clean();
     $data = _templateData('data');
-    $data[$name] = $data[$name] . $content;
+    $data[$name] = $data[$name].$content;
     _templateData('data', $data);
     unset($sections[$name]);
+
     return true;
 }
 
+/**
+ * @param string $value
+ *
+ * @return string
+ */
 function escape($value)
 {
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
