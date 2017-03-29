55d6736918d5c3051989ca567ff763ad6ddeca5a
sinus86 <fjodor.m@web.de>
2017-03-29 23:27:22 +0200

    Added module function, updated module bootstrapping and some cosmetic changes done

--- config/database.example.php
+++ config/database.example.php
@@ -1,12 +1,13 @@
 <?php
 
-config('db',
-    [
-        'host' => 'localhost',
-        'username' => 'username',
-        'password' => 'password',
-        'database' => 'webrpg',
-        'port' =>3306,
-        'charset' => 'utf-8'
-    ]
-);
+/**
+ * Local database configuration
+ */
+config('db', [
+    'host' => 'localhost',
+    'username' => 'username',
+    'password' => 'password',
+    'database' => 'webrpg',
+    'port' => 3306,
+    'charset' => 'utf-8'
+]);
---
+++ config/modules.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Enable modules
+ */
+module('account', 'bootstrap.php');     // load account module
+module('character', 'bootstrap.php');   // load character module
+module('inventory', 'bootstrap.php');   // load inventory module
+module('map', 'bootstrap.php');         // load map module
