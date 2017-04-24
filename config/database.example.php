c4b9d5a09b11cb819e90bbeeb4451ed9a5e4e5db
BlackScorp <witalimik@web.de>
2017-04-24 15:04:26 +0200

    restored example file

---
+++ config/database.example.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Local database configuration
+ */
+config('db', [
+    'host' => 'localhost',
+    'username' => 'username',
+    'password' => 'password',
+    'database' => 'webrpg',
+    'port' => 3306,
+    'charset' => 'utf-8'
+]);
