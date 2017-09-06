2d437f67037f4305bbbd67da3e63e58d9dd473a7
Artur <0x00.dev@gmail.com>
2017-09-07 00:28:32 +0500

    Small refactoring
    Replaced string concatenations to sprintf functions

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ ini_set('display_errors', true);
 
 $scriptUrl = '/';
 $beforeIndexPosition = strpos($_SERVER['PHP_SELF'], '/index.php');
-if (false !== $beforeIndexPosition && $beforeIndexPosition > 0) {
-    $scriptUrl = substr($_SERVER['PHP_SELF'], 0, $beforeIndexPosition).'/';
+if ($beforeIndexPosition && $beforeIndexPosition > 0) {
+    $scriptUrl = sprintf('%s/', substr($_SERVER['PHP_SELF'], 0, $beforeIndexPosition));
     $_SERVER['REQUEST_URI'] = str_replace(['/index.php', $scriptUrl], '/', $_SERVER['REQUEST_URI']);
 }
 define('BASE_DIR', $scriptUrl);
 
-require_once __DIR__ . '/bootstrap.php';
+require_once sprintf('%s/bootstrap.php',__DIR__);
 
 
 
