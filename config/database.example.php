fa5f2ae345dd3bcd29b67bae9610eb17426ca05d
BlackScorp <witalimik@web.de>
2019-07-29 20:28:20 +0200

    added database example

---
+++ config/database.example.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Local database configuration
+ */
+config('db', [
+    'host' => 'localhost',
+    'username' => 'username',
+    'password' => 'password',
+    'database' => 'database',
+    'port' => 3306,
+    'charset' => 'utf-8'
+]);
